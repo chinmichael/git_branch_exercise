aca0e198eb57f1cc90cbd22fb79bf49b04210cd4
chinmichael <chinmichael@naver.com>
2021-04-14 22:15:13 +0900

    .gitignore, master branch setting

---
+++ src/main/java/com/git_example/git_branch_example/GitBranchExampleApplication.java
@@ -0,0 +1,13 @@
+package com.git_example.git_branch_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBranchExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitBranchExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git_example/git_branch_example/GitBranchExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git_example.git_branch_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitBranchExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
